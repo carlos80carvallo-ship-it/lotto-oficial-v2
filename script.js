3f158b8130ddfc4a7d57bf33ae10d4a88c875afa
carlos80carvallo-ship-it <carlos80carvallo@gmail.com>
2026-03-12 05:23:31 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function moverReloj() {
-    const ahora = new Date();
-    const tiempo = ahora.toLocaleTimeString();
-    if(document.getElementById('reloj')) {
-        document.getElementById('reloj').textContent = tiempo;
-    }
-}
-setInterval(moverReloj, 1000);
-moverReloj();
